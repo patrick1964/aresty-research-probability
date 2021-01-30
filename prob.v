05b08171194cfdadbb2686faf22f3536675d6743
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2021-01-30 13:04:46 -0500

    Add elimination rule

--- prob.v
+++ prob.v
@@ -1,6 +1,27 @@
-Inductive Prob {X : Type} : Type :=
+(*
+Formation Rule
+A : Type
+________
+Prob(A) : Type
+
+Introduction Rule
+a : A => evid(a) : Prob(A)
+
+Elimination Rule
+a : A => f(a) : B
+__________________
+x : Prob(A) => imp_f(x) : Prob(B)
+
+Computation Rule
+imp_f(evid_A(a)) = evid_B(f(a))
+*)
+
+(* Formation Rule, Introduction Rule *)
+Inductive Prob (X : Type): Type :=
   | evid (x : X).
 
-Axiom inference : forall A B : Type,
-  ((exists a : A, True) -> (exists b : B, True))
-  -> (exists b : B, True) -> (exists a : @Prob A, True).
+Axiom elimination_rule :
+  forall (A B : Type) (a : A) (f : A -> B),
+  (forall x : Prob A, (exists impf_x : Prob B, True)).
+
+
