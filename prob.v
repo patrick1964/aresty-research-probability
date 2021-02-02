4383ac41ca9f60e41ee0fcaa8496395b138a8af3
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2021-02-02 09:27:12 -0500

    Modify axioms for probability type

--- prob.v
+++ prob.v
@@ -20,14 +20,51 @@ imp_f(evid_A(a)) = evid_B(f(a))
 Inductive Prob (X : Type): Type :=
   | evid (x : X).
 
+(*
+Theorem imp_as_theorem :
+  forall (A B : Type) (f : A -> B), (Prob A) -> (Prob B).
+Proof.
+  intros A B f probA.
+  destruct probA.
+  apply f in x.
+  apply evid in x.
+  apply x.
+Qed.
+*)
+
+(* Using match with Prob is implicitly assuming that we used evid as the
+   constructor - don't do that. *)
+Print imp_as_theorem.
+
+Print Prob_rect.
+Print Prob_ind.
+Print Prob_rec.
+Print Prob_sind.
+
+Check evid nat 2.
+
+(* Old Elimination Rule
 (* Is this needed, or is the computation rule enough? *)
 Axiom elimination_rule :
   forall (A B : Type) (a : A) (f : A -> B),
   (forall x : Prob A, (exists impf : (Prob A) -> (Prob B), True)).
-  
+*)
+
+(* Equivalent to elimination rule *)
+Axiom imp :
+  forall (A B : Type) (f : A -> B), (Prob A) -> (Prob B).
+
+Check imp nat bool (fun (n: nat) => true) (evid nat 1).
+
+Axiom computation_rule :
+  forall (A B : Type) (f : A -> B) (a: A),
+  imp A B f (evid A a) = evid B (f a).
+
+(* Old computation rule
 (* Should the forall a be before the impf part? *)
 Axiom computation_rule :
   forall (A B : Type) (a : A) (f : A -> B),
   exists (impf : (Prob A) -> (Prob B)), (impf (evid A a)) = (evid B (f a)).
+*)
   
   
