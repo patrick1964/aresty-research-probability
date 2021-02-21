08bc30a29707d1ab5f7c065dd94d46345f0bdd3a
Jonathan-Ackerman <jonathanackerman01@gmail.com>
2021-02-21 12:50:06 -0500

    Finish independent types versions of rules

--- prob.v
+++ prob.v
@@ -42,36 +42,10 @@ Print Prob_sind.
 
 Check evid nat 2.
 
-(* Old Elimination Rule
-(* Is this needed, or is the computation rule enough? *)
-Axiom elimination_rule :
-  forall (A B : Type) (a : A) (f : A -> B),
-  (forall x : Prob A, (exists impf : (Prob A) -> (Prob B), True)).
-*)
-
-(* Equivalent to elimination rule *)
-(*
-Axiom prob_imp :
-  forall (A B : Type) (f : A -> B), (Prob A) -> (Prob B).
-*)
-
 Axiom prob_imp :
   forall (A : Type) (B : (Prob A) -> Type)
   (f: forall a : A, B (evid A a)),
   forall x : Prob A, Prob (B x).
-
-Theorem prob_imp_independent :
-  forall (A B : Type) (f : A -> B), (Prob A) -> (Prob B).
-Proof.
-  intros A B f.
-  apply (prob_imp A (fun (_ : Prob A) => B) f).
-Qed.
-
-(*
-Axiom prob_comp :
-  forall (A B : Type) (f : A -> B) (a: A),
-  prob_imp A B f (evid A a) = evid B (f a).
-*)
 
 Axiom prob_comp :
   forall (A : Type) (B : (Prob A) -> Type)
@@ -79,47 +53,23 @@ Axiom prob_comp :
   (a : A),
   prob_imp A B f (evid A a) = evid (B (evid A a)) (f a).
 
-(*
-TODO
-Theorem prob_comp_independent :
-  forall (A B : Type) (f : A -> B) (a: A),
-  prob_imp_independent A B f (evid A a) = evid B (f a).
-Proof.
-  intros A B f a.
-  unfold prob_imp_independent.
-  rewrite <- (prob_comp A (fun (_ : Prob A) => B) f a).
-  apply prob_imp.
-  rewrite <- prob_imp_independent.
-*)
-  
-
 (* TODO think about whether this is necessary? *)
 Axiom prob_function_extensionality :
   forall (A : Type) (B : Type) (f1 : Prob A -> B) (f2 : Prob A -> B),
   (forall (a : A), (f1 (evid A a)) = (f2 (evid A a))) -> f1 = f2.
 
-(* Goal: When B is a non-dependent type,
-the two elimination rules are the same.
-*)
-(*
-Theorem prob_imp_equivalence : 
-  forall (A B : Type) (f : A -> B),
-  prob_imp A B f =
-  prob_imp A (fun (_ : Prob A) => B)
-  (fun (a : A) => (f a)).
-Proof.
-  intros A B f.
-  apply prob_function_extensionality.
-  intros a.
-  rewrite prob_comp.
-  rewrite prob_comp.
-  reflexivity.
-Qed.
-*)
+Definition prob_imp_independent (A B : Type) (f : A -> B)
+  := prob_imp A (fun (_ : Prob A) => B) f.
 
-(*
-Does this violate injectivity?
-*)
+Theorem prob_comp_independent :
+  forall (A B : Type) (f : A -> B) (a: A),
+  prob_imp_independent A B f (evid A a) = evid B (f a).
+Proof.
+  intros A B f a.
+  rewrite <- (prob_comp A (fun (_ : Prob A) => B) f a).
+  reflexivity.
+Qed. 
+
 Inductive ProbLevel (X : Type) (level : nat) : Type :=
   | absolute_evid (x : X)
   | prob_evid (p : ProbLevel X (level + 1)).
@@ -130,18 +80,6 @@ Check absolute_evid nat 2 1.
 (* Create a ProbLevel using probabilistic evidence from 1 level higher *)
 Check prob_evid nat 2 (absolute_evid nat 3 1).
 
-(* Two Elimination Rules *)
-(*
-Axiom prob_level_abs_imp :
-  forall (A B : Type) (f : A -> B) (n : nat),
-  (ProbLevel A n) -> (ProbLevel B n).
-  
-Axiom prob_level_evid_imp :
-  forall (A B : Type) (m n : nat),
-  ((ProbLevel A (m + 1)) -> (ProbLevel B (n + 1))) ->
-  ((ProbLevel A m) -> (ProbLevel B n)).
-*)
-
 Axiom prob_level_abs_imp :
   forall (n : nat) (A : Type) (B: (ProbLevel A n) -> Type)
   (f : forall (a : A), B (absolute_evid A n a)),
@@ -151,21 +89,6 @@ Axiom prob_level_evid_imp :
   forall (m n : nat) (A : Type) (B: (ProbLevel A m) -> Type)
   (f : forall (p : ProbLevel A (m + 1)), ProbLevel (B (prob_evid A m p)) (n + 1)),
   forall (p : ProbLevel A m), (ProbLevel (B p) n).
-
-(* Two Computation Rules *)
-(*
-Axiom prob_level_abs_comp :
-  forall (A B : Type) (f : A -> B) (n : nat)
-  (a: A),
-  prob_level_abs_imp A B f n (absolute_evid A n a)
-  = absolute_evid B n (f a).
-
-Axiom prob_level_evid_comp :
-  forall (A B : Type) (m n : nat) (p: ProbLevel A (m + 1))
-  (f : (ProbLevel A (m + 1)) -> (ProbLevel B (n + 1))),
-  prob_level_evid_imp A B m n f (prob_evid A m p)
-  = prob_evid B n (f p).
-*)
 
 Axiom prob_level_abs_comp :
   forall (n : nat) (A : Type) (B: (ProbLevel A n) -> Type)
@@ -179,7 +102,42 @@ Axiom prob_level_evid_comp :
   forall (p : ProbLevel A (m + 1)),
   prob_level_evid_imp m n A B f (prob_evid A m p)
   = prob_evid (B (prob_evid A m p)) n (f p).
+  
+Definition prob_level_abs_imp_independent (A B : Type) (f : A -> B) (n : nat)
+  := (prob_level_abs_imp n A (fun (_ : ProbLevel A n) => B) f).
+  
+Definition prob_level_evid_imp_independent (A B : Type) (m n : nat)
+  (f : (ProbLevel A (m + 1)) -> (ProbLevel B (n + 1)))
+  := prob_level_evid_imp m n A (fun (_ : ProbLevel A m) => B) f.
 
+Definition prob_level_abs_comp_indepenent (A B : Type) (f : A -> B) (n : nat) (a : A)
+  := prob_level_abs_comp n A (fun (_ : ProbLevel A n) => B) f.
+
+Theorem prob_level_abs_comp_independent_thm :
+  forall (A B : Type) (f : A -> B) (n : nat)
+  (a: A),
+  prob_level_abs_imp_independent A B f n (absolute_evid A n a)
+  = absolute_evid B n (f a).
+Proof.
+  intros.
+  rewrite <- (prob_level_abs_comp n A (fun (_ : ProbLevel A n) => B) f).
+  reflexivity.
+Qed.
+
+Definition prob_level_evid_comp_independent (A B : Type) (m n : nat)
+  (p : ProbLevel A (m + 1)) (f : (ProbLevel A (m + 1)) -> (ProbLevel B (n + 1)))
+  := prob_level_evid_comp m n A (fun (_ : ProbLevel A m) => B) f.
+
+Theorem prob_level_evid_comp_independent_thm :
+  forall (A B : Type) (m n : nat) (p: ProbLevel A (m + 1))
+  (f : (ProbLevel A (m + 1)) -> (ProbLevel B (n + 1))),
+  prob_level_evid_imp_independent A B m n f (prob_evid A m p)
+  = prob_evid B n (f p).
+Proof.
+  intros.
+  rewrite <- (prob_level_evid_comp m n A (fun (_ : ProbLevel A m) => B) f).
+  reflexivity.
+Qed.
 
 (* TODO do we need both parts of the /\? *)
 (*
@@ -223,10 +181,14 @@ Proof.
   destruct H.
     - Abort.
 
+Theorem prob_pair_to_union :
+  forall (A B : Type), ((Prob A) * (Prob B)) -> ((Prob A) + (Prob B)).
+Proof.
+  Abort.
+
 (*
 TODO (more specific things)
   - try to finish the placeholder theorem (if possible)
-  - make independent types theorem in terms of dependent types
   - a few other theorems I wrote down
 *)
 
